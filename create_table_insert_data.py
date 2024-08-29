d0e4eaf6fe2f02e2e9003fb8a19090fc796c2742
Kofitelli <92218419+Kofitelli@users.noreply.github.com>
2024-08-29 14:30:39 +0000

    Update create_table_insert_data.py

--- create_table_insert_data.py
+++ create_table_insert_data.py
@@ -1,7 +1,4 @@
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-
+from dag_fxn import *
 
 default_args = {
     'owner': 'nsano',
